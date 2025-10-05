703f5661d71ba3e79f25cd7520c95a4b5156f301
lohan.cravo <lohan.cravo@monest.com.br>
2025-10-04 21:21:47 -0300

    Add in-memory service registry

---
+++ pkg/discovery/memory/memory.go
@@ -0,0 +1,92 @@
+package memory
+
+import (
+	"context"
+	"errors"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/lohanguedes/movie-microservices/pkg/discovery"
+)
+
+type Registry struct {
+	sync.RWMutex
+	serviceAddrs map[string]map[string]*serviceInstance
+}
+
+type serviceInstance struct {
+	hostPort   string
+	lastActive time.Time
+}
+
+func NewRegistry() *Registry {
+	return &Registry{
+		serviceAddrs: map[string]map[string]*serviceInstance{},
+	}
+}
+
+func (r *Registry) Register(
+	ctx context.Context,
+	instanceID string,
+	serviceName string,
+	hostPort string,
+) error {
+	r.Lock()
+	defer r.Unlock()
+	if _, ok := r.serviceAddrs[serviceName]; !ok {
+		r.serviceAddrs[serviceName] = map[string]*serviceInstance{}
+	}
+	r.serviceAddrs[serviceName][instanceID] = &serviceInstance{hostPort: hostPort, lastActive: time.Now()}
+	return nil
+}
+
+func (r *Registry) Deregiste(
+	ctx context.Context,
+	instanceID string,
+	serviceName string,
+) error {
+	r.Lock()
+	defer r.Unlock()
+	if _, ok := r.serviceAddrs[serviceName]; !ok {
+		return nil
+	}
+	delete(r.serviceAddrs[serviceName], instanceID)
+	return nil
+}
+
+func (r *Registry) ReportHealhyState(instanceID string, serviceName string) error {
+	r.Lock()
+	defer r.Unlock()
+	if _, ok := r.serviceAddrs[serviceName]; !ok {
+		return errors.New("service not registered")
+	}
+
+	if _, ok := r.serviceAddrs[serviceName][instanceID]; !ok {
+		return errors.New("instance" + instanceID + " of service" + serviceName + " is not registered")
+	}
+
+	r.serviceAddrs[serviceName][instanceID].lastActive = time.Now()
+	return nil
+}
+
+func (r *Registry) ServiceAddresses(
+	ctx context.Context,
+	serviceID string,
+) ([]string, error) {
+	r.RLock()
+	defer r.RUnlock()
+	if _, ok := r.serviceAddrs[serviceID]; !ok {
+		return nil, discovery.ErrNotFound
+	}
+
+	res := []string{}
+	for instanceID, instance := range r.serviceAddrs[serviceID] {
+		if instance.lastActive.Before(time.Now().Add(-5 * time.Second)) {
+			log.Println("Instance " + instanceID + "of service" + serviceID + " is not active, skipping")
+			continue
+		}
+		res = append(res, instance.hostPort)
+	}
+	return res, nil
+}
